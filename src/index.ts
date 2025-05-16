f1a925103fb99f267155dd4ed2ef16ebcab41a7d
Patrick Bowen <phunanon@users.noreply.github.com>
2025-05-16 13:22:05 +0100

    Measure deletes per hour

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Client, GatewayIntentBits, Message } from 'discord.js';
+import { ActivityType, ChannelType, Client, GatewayIntentBits, Message } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -26,25 +26,35 @@ if (!TOKEN || !CHANNEL_SF || !OLDEST_SF) {
       return;
     }
 
+    const begun = Date.now();
+    let numDeleted = 0;
     while (true) {
       const oldestMessages = [
         ...(await channel.messages
           .fetch({ limit: 100, around: OLDEST_SF })
           .then(messages => messages.filter(m => m.id !== OLDEST_SF).values())),
       ];
-        await new Promise(resolve => setTimeout(resolve, 1_000));
+      await new Promise(resolve => setTimeout(resolve, 1_000));
       const olderThanThirtyDays = oldestMessages.filter(
         m => Date.now() - m.createdTimestamp > thirtyDaysInMilliseconds,
       );
+      let perHour = 0;
       for (const m of olderThanThirtyDays) {
         try {
           const messageToDelete = await channel.messages.fetch(m.id.toString());
           if (messageToDelete) {
             await messageToDelete.delete();
-            console.log(`Deleted ${fmt(messageToDelete.createdTimestamp)}`);
+            const when = fmt(messageToDelete.createdTimestamp);
+            ++numDeleted;
+            perHour = numDeleted / ((Date.now() - begun) / 3_600_000);
+            console.log(`Deleted ${when} @ ${Math.ceil(perHour)} per hour`);
           }
         } catch {}
         await new Promise(resolve => setTimeout(resolve, 2_000));
+        client.user?.setActivity(
+          `Deleting ${Math.ceil(perHour)} messages per hour`,
+          { type: ActivityType.Custom },
+        );
       }
     }
   });
